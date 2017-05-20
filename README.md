f536f964c802b2834a3e1a5415be88f7aa2a2e94
ComeDownToUs <blues4ceausescu@gmail.com>
2017-05-20 10:22:48 +0100

    styled button a bit more, improved performance, added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+Very simple chrome extension for Last.FM.
+
+The delete scrobble button was moved in the redesign last year to be under a drop down menu. As the system frequently will record things as being played which weren't, this is pretty awkward for tidying up records. This extension scrapes the button and its respective form from the drop down menu and plants it roughly in the same location it used to be.
+
+The code currently runs on every last.fm library page which may have deletion options (URLs specified in the manifest) and only makes changes on pages which do have them. The code breaks early when it realises you're on a page where you do not have deletion privileges but probably could be improved.
+
+
+Things to do:
+- Design an icon
+- Style the button to behave exactly like the old one
+- Add a pop up form to ensure it only checks on pages with your username
--- delete-code.js
+++ delete-code.js
@@ -2,14 +2,13 @@ var get_delete_form = function(scrobble_row){
   var drop_down_forms = scrobble_row
     .getElementsByClassName('chartlist-more-menu')[0]
     .getElementsByTagName('form');
-  console.log('1');
   for(var i = 0; i < drop_down_forms.length; i++){
-    console.log(i);
     if(drop_down_forms[i].getAttribute('action').indexOf('unscrobble')!== -1){
       var delete_form = drop_down_forms[i].cloneNode(true);
-      delete_form
-      .getElementsByClassName('mimic-link dropdown-menu-clickable-item')[0]
-      .innerHTML = '✖';
+      var delete_button = delete_form
+          .getElementsByClassName('mimic-link dropdown-menu-clickable-item')[0];
+      delete_button.innerHTML = '╳';
+      delete_button.title = 'Delete Scrobble';
       return delete_form;
     }
   }
@@ -19,14 +18,14 @@ var get_delete_form = function(scrobble_row){
 var scrobbles = document.getElementsByClassName('js-link-block js-focus-controls-container');
 
 for(var i_scrob = 0; i_scrob < scrobbles.length; i_scrob++){
-  console.log(i_scrob);
   var placement_cell = document.createElement('TD')
   placement_cell.className ='chrome-extension-delete';
   var delete_button = get_delete_form(scrobbles[i_scrob]);
   if (delete_button !== null){
     placement_cell.appendChild(delete_button);
     scrobbles[i_scrob].appendChild(placement_cell);
-
+  }else{
+    break; //absolutely no reason going through the whole page if it's not your library page
   }
 }
 
